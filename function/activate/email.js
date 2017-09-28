20f5cc20e7759f7093abf07b535f314273007f8b
孟政元 <278227739@qq.com>
2017-09-28 13:26:34 +0800

    /activate/email

---
+++ function/activate/email.js
@@ -0,0 +1,36 @@
+const util = require('../../lib/util');
+const persistence = require('../../lib/persistence/action');
+const koa = require('../../koa');
+const _email = require('../../lib/cache/action/activate/email');
+
+module.exports= async function(ctx)
+{
+    const query = ctx.query;
+    if( ! (query.session))
+    {
+        ctx.body =
+            {
+                status: 'param_wrong'
+            };
+        return;
+    }
+    const email = await _email.getEmailFromSession(query.session);
+    if(email === null)
+    {
+        ctx.body =
+            {
+                status: 'session_expired',
+            };
+    }
+    else
+    {
+        await persistence.activateEmail(email);
+        // await _email.deleteSession(query.session);
+        ctx.body =
+            {
+                status: 'ok',
+            };
+    }
+};
+
+const app = koa(module.exports, 60006);
