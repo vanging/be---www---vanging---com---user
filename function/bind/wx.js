032713bf4e677462ea1851e3376e5ba3400be30f
孟政元 <278227739@qq.com>
2017-10-02 21:27:02 +0800

    template and app name

---
+++ function/bind/wx.js
@@ -0,0 +1,27 @@
+const session = require('../../middleware/login/session');
+const persistence = require('../../lib/persistence/action');
+const koa = require('../../koa');
+const port = require('../port');
+
+module.exports= async function(ctx)
+{
+    try
+    {
+        const profile = await persistence.findProfileFromUid(ctx.state.uid);
+        ctx.body =
+            {
+                status: 'ok',
+                message: profile
+            };
+    }
+    catch(e)
+    {
+        console.log(e);
+        ctx.body =
+            {
+                status: 'db_error'
+            }
+    }
+};
+
+const app = koa(module.exports, port.bind.wx, [session]);
